0bc85a54f13bd571b0a1efad1969f917bb639f2c
Tomas <tomas.adomavicius@teo.lt>
2016-05-25 15:41:10 +0300

    added comments file updated to latest Vygintas code

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM frolvlad/alpine-mono:latest
 RUN mkdir /home/app
 COPY . /home/app
+
+
+
 CMD mono /home/app/bin/Debug/docker01.exe
 
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Text;
 using Newtonsoft.Json.Linq;
 using RabbitMQ.Client;
@@ -8,79 +8,73 @@ using System.Threading;
 
 namespace WafflesMinion
 {
-    class Program
-    {
-        public static void Main(string[] args)
-        { 
-            var secs = 2;
+	class Program
+	{
+		public static void Main(string[] args)
+		{ 
+			var secs = 2;
 
-            Console.WriteLine(System.Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"));
-            Console.WriteLine(System.Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD"));
-            Thread.Sleep(5000);
-            
-            var factory = new ConnectionFactory() { HostName = "rabbitmq.cloud66.local", 
-                UserName = System.Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
-                Password = System.Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") 
-            };
-            //Console.WriteLine("A");
-            using (var connection = factory.CreateConnection())
-            {
-                using (var ch_in = connection.CreateModel())
-                using (var ch_out = connection.CreateModel())
-                {
-                	//Console.WriteLine("B");
-                                ch_in.QueueDeclare(queue: "bakery.waffle.order",
+			Console.WriteLine(System.Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"));
+			Console.WriteLine(System.Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD"));
+			Thread.Sleep(5000);
+			
+			var factory = new ConnectionFactory() { HostName = "rabbitmq.cloud66.local", 
+				UserName = System.Environment.GetEnvironmentVariable("RABBITMQ_USERNAME"),
+				Password = System.Environment.GetEnvironmentVariable("RABBITMQ_PASSWORD") 
+			};
+	        using (var connection = factory.CreateConnection())
+	        {
+	        	using (var ch_in = connection.CreateModel())
+	        	using (var ch_out = connection.CreateModel())
+	            {
+	        		            ch_in.QueueDeclare(queue: "bakery.wafer.order",
                                  durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
-                                ch_out.QueueDeclare(queue: "bakery.sweet.done",
+	        		            ch_out.QueueDeclare(queue: "bakery.wafer.done",
                                  durable: true,
                                  exclusive: false,
                                  autoDelete: false,
                                  arguments: null);
-                	//Console.WriteLine("C");
-                    ch_in.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
-                    Console.Write(System.Environment.MachineName + ":-- [*] Waiting for waffle back orders (backing time = " + secs + ")");
-					
-                    //Console.WriteLine("C");
-                    var consumer = new EventingBasicConsumer(ch_in);
-                    consumer.Received += (model, ea) =>
-                    {
-                        var body = ea.Body;
-                        var message = Encoding.UTF8.GetString(body);
-//                        dynamic data = JValue.Parse(message);
-                        JToken token = JObject.Parse(message);
-                        var flavour =  (string)token.SelectToken("flavour");
-                        Console.WriteLine(" [x] Received {0}", message);
-        
-                        Console.WriteLine(System.Environment.MachineName + ":-- [.] Start backing a " + flavour + " waffle");
-                        Thread.Sleep(secs * 1000);
-                        Console.WriteLine(System.Environment.MachineName + ":-- [.] Start backing a " + flavour + " done");
-        
-                        ch_in.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
-//                        ch_out..sendToQueue('bakery.sweet.done', new Buffer(msg.content), {persistent: true});
-                        var properties = ch_out.CreateBasicProperties();
-                        properties.SetPersistent(true);
+	        		ch_in.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+	        		Console.Write(System.Environment.MachineName + ":-- [*] Waiting for waffles back orders (backing time = " + secs + ")");
 
-                        ch_out.BasicPublish(exchange: "",
-                                             routingKey: "bakery.sweet.done",
-                                             basicProperties: properties,
-                                             body: body);
-                                };
+	        		var consumer = new EventingBasicConsumer(ch_in);
+		            consumer.Received += (model, ea) =>
+		            {
+		                var body = ea.Body;
+		                var message = Encoding.UTF8.GetString(body);
+		                JToken token = JObject.Parse(message);
+		                var flavour =  (string)token.SelectToken("flavour");
+		                Console.WriteLine(" [x] Received {0}", message);
+		
+		                Console.WriteLine(System.Environment.MachineName + ":-- [.] Start backing a " + flavour + " wafer");
+		                Thread.Sleep(secs * 1000);
+		                Console.WriteLine(System.Environment.MachineName + ":-- [.] Start backing a " + flavour + " done");
+		
+						ch_in.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+						var properties = ch_out.CreateBasicProperties();
+            			properties.SetPersistent(true);
+
+			            ch_out.BasicPublish(exchange: "",
+			                                 routingKey: "bakery.wafer.done",
+			                                 basicProperties: properties,
+			                                 body: body);
+					            };
 
 
-                    ch_in.BasicConsume(queue: "bakery.waffle.order",
-                                         noAck: false,
-                                         consumer: consumer);
-        
-                }
-                //Console.WriteLine("D");
-            }            
-            //Console.WriteLine("E");
-            
-//            Console.Write("Press any key to continue . . . ");
-            Console.ReadKey(true);
-        }
-    }
+		            ch_in.BasicConsume(queue: "bakery.wafer.order",
+		                                 noAck: false,
+		                                 consumer: consumer);
+		
+		            Console.WriteLine(" Press [enter] to exit.");
+		            Console.ReadLine();
+	        	}
+	        }			
+			
+			Console.Write("EXIT");
+//			Console.ReadKey(true);
+		}
+	}
 }
